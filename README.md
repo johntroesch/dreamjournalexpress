381b989d4a7bee61e269f5b3895c1f9601772d37
unknown <johntroesch@gmail.com>
2021-01-27 16:48:04 -0800

    header image added

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 Dream Journal with Express and MongoDB
 
 default start: SET DEBUG=dreamjournalexpress:* | npm start
+Nodemon: SET DEBUG=dreamjournalexpress:* | npm run devstart
--- models/user.js
+++ models/user.js
@@ -4,9 +4,7 @@ var Schema = mongoose.Schema;
 
 var UserSchema = new Schema({
     username: {type: String, required: true, maxlength: 20},
-    password: {type: String, required: true, maxlength: 40},
-    first_name: {type: String, required: true, maxlength: 20},
-    last_name: {type: String, required: true, maxlength: 20}
+    password: {type: String, required: true, minlength: 8, maxlength: 40}
 })
 
 module.exports = mongoose.model('User', UserSchema);
